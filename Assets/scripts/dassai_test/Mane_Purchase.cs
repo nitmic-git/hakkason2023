d27482599f925ec2bd5e038f2aca90eeaa408f05
dassai0079 <st3iwa@gmail.com>
2024-01-09 17:33:46 +0900

    add_shopsystem
    なんかいろいろやった。
    装備の購入システムを完成させました
    購入すると勝手に装備される仕組みです
    あとスキル購入と装備・アイテム購入を分離させました
    スクリプトの名前にpurchaseが多くなって大氾濫してるよ...

---
+++ Assets/scripts/dassai_test/Mane_Purchase.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mane_Purchase : MonoBehaviour
+{
+    public GameObject[] Item = new GameObject[30];
+    int selecter1;
+    int selecter2;
+    int selecter3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 0; i <= 12; i++)
+        {
+            Item[i].SetActive(false);
+            Debug.Log(i + "が非アクティブ化");
+        }
+        selecter1 = Random.Range(0, 3);
+        selecter2 = Random.Range(0, 3) + 3;
+        selecter3 = Random.Range(0, 3) + 5;
+        Item[selecter1].SetActive(true);
+        Item[selecter2].SetActive(true);
+        Item[selecter3].SetActive(true);
+    }
+
+    void Update()
+    {
+        
+    }
+}
