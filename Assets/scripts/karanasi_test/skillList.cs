186dbfecea35764489b711c21ddb86831bcf755a
karanasinoki <132255271+karanasinoki@users.noreply.github.com>
2024-01-10 13:02:13 +0900

    skill4

--- Assets/scripts/karanasi_test/skillList.cs
+++ Assets/scripts/karanasi_test/skillList.cs
@@ -6,6 +6,8 @@ public class skillList : MonoBehaviour
 {
     [SerializeField] GameObject arrowNoGra;
     [SerializeField] GameObject unit;
+    [SerializeField] GameObject fireAxis;
+    [SerializeField] float fireRotateTime;
 
     public void SpeedUp_00()
     {
@@ -17,11 +19,30 @@ public class skillList : MonoBehaviour
         Instantiate(arrowNoGra, unit.transform.position, Quaternion.identity);
     }
 
+    public void fireRotate_02()
+    {
+        StartCoroutine(active(fireAxis));
+    }
+
+    public IEnumerator active(GameObject obj)
+    {
+        obj.SetActive(true);
+        yield return new WaitForSeconds(fireRotateTime);
+        obj.SetActive(false);
+    }
+
+
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             HolizonArrow();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            fireRotate_02();
         }
     }
 }
